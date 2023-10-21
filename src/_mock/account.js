8251b93a55caea2bfae40da04c4cb4fcc5b3e116
salik03 <uddinsalik03@gmail.com>
2023-10-21 20:19:09 +0530

    first commit

---
+++ src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Salik Traders Co.',
+  email: 'saliktradersco@dnk.com',
+  photoURL: '/assets/images/avatars/demo.jpg',
+};
