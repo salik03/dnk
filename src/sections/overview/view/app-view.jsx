aa4fe79432399ff0aa7a44ce78d531ab476a3aef
salik03 <uddinsalik03@gmail.com>
2023-10-24 18:08:56 +0530

    navbar setup

--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -22,7 +22,7 @@ export default function AppView() {
   return (
     <Container maxWidth="xl">
       <Typography variant="h4" sx={{ mb: 5 }}>
-        Hi, Welcome back 👋
+        Your Summary
       </Typography>
 
       <Grid container spacing={3}>
