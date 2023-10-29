10605c4631cda52949e36b73cda4c332fe28ec52
Arnav Kumar Mittal <arnavmittal17@gmail.com>
2023-10-30 01:24:15 +0530

    Changes in Dashboard

--- src/sections/homepage/view/app-view.jsx
+++ src/sections/homepage/view/app-view.jsx
@@ -4,7 +4,7 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
 
-import Iconify from 'src/components/iconify';
+// import Iconify from 'src/components/iconify';
 
 import AppTasks from '../app-tasks';
 import AppNewsUpdate from '../app-news-update';
@@ -12,8 +12,8 @@ import AppOrderTimeline from '../app-order-timeline';
 import AppCurrentVisits from '../app-current-visits';
 import AppWebsiteVisits from '../app-website-visits';
 import AppWidgetSummary from '../app-widget-summary';
-import AppTrafficBySite from '../app-traffic-by-site';
-import AppCurrentSubject from '../app-current-subject';
+// import AppTrafficBySite from '../app-traffic-by-site';
+// import AppCurrentSubject from '../app-current-subject';
 import AppConversionRates from '../app-conversion-rates';
 
 // ----------------------------------------------------------------------
@@ -26,39 +26,57 @@ export default function Homepage() {
       </Typography>
 
       <Grid container spacing={3}>
-        <Grid xs={12} sm={6} md={3}>
+        <Grid xs={12} sm={6} md={3.5}>
           <AppWidgetSummary
-            title="Weekly Sales"
+            title="PO induction pending"
             total={714000}
             color="success"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
           />
         </Grid>
 
-        <Grid xs={12} sm={6} md={3}>
+        <Grid xs={12} sm={6} md={3.5}>
           <AppWidgetSummary
-            title="New Users"
+            title="In transit"
             total={1352831}
             color="info"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
           />
         </Grid>
 
-        <Grid xs={12} sm={6} md={3}>
+        <Grid xs={12} sm={6} md={3.5}>
           <AppWidgetSummary
-            title="Item Orders"
+            title="Under process @ Customs"
             total={1723315}
             color="warning"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
           />
         </Grid>
 
-        <Grid xs={12} sm={6} md={3}>
+        <Grid xs={12} sm={6} md={3.5}>
           <AppWidgetSummary
-            title="Bug Reports"
+            title="Customs cleared"
             total={234}
             color="error"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={3.5}>
+          <AppWidgetSummary
+            title="Customs detained"
+            total={1352831}
+            color="info"
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={3.5}>
+          <AppWidgetSummary
+            title="Despatched out of India"
+            total={1723315}
+            color="warning"
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
           />
         </Grid>
 
@@ -87,12 +105,7 @@ export default function Homepage() {
                   fill: 'solid',
                   data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
                 },
-                {
-                  name: 'Team B',
-                  type: 'area',
-                  fill: 'gradient',
-                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
-                },
+                
                 {
                   name: 'Team C',
                   type: 'line',
@@ -109,10 +122,13 @@ export default function Homepage() {
             title="Current Visits"
             chart={{
               series: [
-                { label: 'America', value: 4344 },
-                { label: 'Asia', value: 5435 },
-                { label: 'Europe', value: 1443 },
-                { label: 'Africa', value: 4443 },
+                { label: 'International EMS Merchandise', value: 4344 },
+                { label: 'International EMS Document', value: 5435 },
+                { label: 'International Regd. Small Packet', value: 1443 },
+                { label: 'International Air Parcel', value: 4443 },
+                { label: 'International RegdL. Letter', value: 4443 },
+                { label: 'International Tracked Packet Service', value: 4443 },
+
               ],
             }}
           />
@@ -120,34 +136,16 @@ export default function Homepage() {
 
         <Grid xs={12} md={6} lg={8}>
           <AppConversionRates
-            title="Conversion Rates"
-            subheader="(+43%) than last year"
+            title="Consignment Exports"
+            subheader="Group by Continents"
             chart={{
               series: [
-                { label: 'Italy', value: 400 },
-                { label: 'Japan', value: 430 },
-                { label: 'China', value: 448 },
-                { label: 'Canada', value: 470 },
-                { label: 'France', value: 540 },
-                { label: 'Germany', value: 580 },
-                { label: 'South Korea', value: 690 },
-                { label: 'Netherlands', value: 1100 },
-                { label: 'United States', value: 1200 },
-                { label: 'United Kingdom', value: 1380 },
-              ],
-            }}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={4}>
-          <AppCurrentSubject
-            title="Current Subject"
-            chart={{
-              categories: ['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math'],
-              series: [
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
+                { label: 'North America', value: 320 },
+                { label: 'Asia', value: 430 },
+                { label: 'Europe', value: 448 },
+                { label: 'Africa', value: 470 },
+                { label: 'South America', value: 540 },
+                { label: 'Australia', value: 350 },
               ],
             }}
           />
@@ -172,10 +170,10 @@ export default function Homepage() {
             list={[...Array(5)].map((_, index) => ({
               id: faker.string.uuid(),
               title: [
-                '1983, orders, $4220',
-                '12 Invoices have been paid',
+                'Order has been placed',
                 'Order #37745 from September',
-                'New order placed #XF-2356',
+                'Order has been Shipped',
+                'Order reached its nearest destination',
                 'New order placed #XF-2346',
               ][index],
               type: `order${index + 1}`,
@@ -184,33 +182,6 @@ export default function Homepage() {
           />
         </Grid>
 
-        <Grid xs={12} md={6} lg={4}>
-          <AppTrafficBySite
-            title="Traffic by Site"
-            list={[
-              {
-                name: 'FaceBook',
-                value: 323234,
-                icon: <Iconify icon="eva:facebook-fill" color="#1877F2" width={32} />,
-              },
-              {
-                name: 'Google',
-                value: 341212,
-                icon: <Iconify icon="eva:google-fill" color="#DF3E30" width={32} />,
-              },
-              {
-                name: 'Linkedin',
-                value: 411213,
-                icon: <Iconify icon="eva:linkedin-fill" color="#006097" width={32} />,
-              },
-              {
-                name: 'Twitter',
-                value: 443232,
-                icon: <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={32} />,
-              },
-            ]}
-          />
-        </Grid>
 
         <Grid xs={12} md={6} lg={8}>
           <AppTasks
@@ -221,6 +192,7 @@ export default function Homepage() {
               { id: '3', name: 'Stakeholder Meeting' },
               { id: '4', name: 'Scoping & Estimations' },
               { id: '5', name: 'Sprint Showcase' },
+              
             ]}
           />
         </Grid>
